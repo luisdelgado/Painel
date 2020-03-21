90eff40b5ef9a4376c8e425c49bf5cf1a15452d4
luisdelgado <luishdelgados10@gmail.com>
2020-03-21 11:11:46 -0300

    Cabeçalho da main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,9 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      O Clube Carnavalesco Misto Elefante de Olinda foi fundado em 12 de fevereiro de 1952.<br>
+      Nosso hino é o hino extraoficial de Olinda.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
