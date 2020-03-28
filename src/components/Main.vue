62885761ef323909c49931354d916b8414b5cf74
luisdelgado <luishdelgados10@gmail.com>
2020-03-28 11:54:07 -0300

    Terminando o hino

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,12 +6,28 @@
       Nosso hino é o hino extraoficial de Olinda.
     </p>
     <h3>Hino do Elefante de Olinda (Olinda Nº2)</h3>
-    <div class="row col-lg-12">
-      <div class="col-lg-6">
-        teste
+    <div class="row col-lg-12 hymn">
+      <div class="col-lg-4 offset-2">
+        <p><i>Ao som dos clarins de Momo<br>
+        O povo aclama com todo ardor<br>
+        O Elefante exaltando as suas tradições<br>
+        E também seu esplendor<br>
+        Olinda, este meu canto<br>
+        Foi inspirado em teu louvor<br>
+        Entre confetes e serpentinas<br>
+        Venho te oferecer<br>
+        Com alegria o meu amor<br><br>
+        Olinda! Quero cantar a ti esta canção<br>
+        Teus coqueirais, o teu sol, o teu mar<br>
+        Faz vibrar meu coração, de amor a sonhar<br>
+        Minha Olinda sem igual<br>
+        Salve o teu Carnaval!</i></p>
+        <p>Clídio Nigro / Clóvis Vieira</p>
       </div>
-      <div class="col-lg-6">
-        teste
+      <div class="col-lg-4 sound">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/tMa67fMqhyo" frameborder="0" 
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+          allowfullscreen></iframe>
       </div>
     </div>
     <h3>Essential Links</h3>
@@ -57,5 +73,11 @@ li {
 }
 a {
   color: #42b983;
+}
+.hymn {
+  margin-top: 3%;
+}
+.sound {
+  margin-top: 2%;
 }
 </style>
