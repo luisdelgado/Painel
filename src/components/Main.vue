b669b1bbbbc751bbc1db8930a173d96dcef64cb5
luisdelgado <luishdelgados10@gmail.com>
2020-04-26 10:05:46 -0300

    Terminando seção Siga o Elefante

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -22,55 +22,56 @@
         </p>
       </div>
     </div>
-    <h3>Hino do Elefante de Olinda</h3>
-    <div v-bind:class="{'row': widthScreen > 1024, 'col-lg-12': true, 'hymn': true}">
-      <div v-bind:class="{'col-lg-4': widthScreen > 1024, 'offset-2' : widthScreen > 1024}">
-        <p><i>Ao som dos clarins de Momo<br>
-        O povo aclama com todo ardor<br>
-        O Elefante exaltando as suas tradições<br>
-        E também seu esplendor<br>
-        Olinda, este meu canto<br>
-        Foi inspirado em teu louvor<br>
-        Entre confetes e serpentinas<br>
-        Venho te oferecer<br>
-        Com alegria o meu amor<br><br>
-        Olinda! Quero cantar a ti esta canção<br>
-        Teus coqueirais, o teu sol, o teu mar<br>
-        Faz vibrar meu coração, de amor a sonhar<br>
-        Minha Olinda sem igual<br>
-        Salve o teu Carnaval!</i></p>
-        <p>Clídio Nigro / Clóvis Vieira</p>
-      </div>
-      <div v-bind:class="{'col-lg-4': widthScreen > 1024, 'sound': true, 'smart': widthScreen <= 414}">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/tMa67fMqhyo" frameborder="0" 
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-          allowfullscreen></iframe>
-      </div>
-    </div>
-    <div class="gallery">
-      <h3>Galeria</h3>
-      <div v-bind:class="{'gallery-time': true, 'row col-lg-12 justify-content-between': widthScreen > 1024}">
-        <img v-bind:class="{'gallery-item': true, 'col-lg-2 first-gallery-item': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" src="./../assets/1953.jpg" />
-        <iframe v-bind:class="{'gallery-item': true, 'col-lg-2': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" width="560" height="315" src="https://www.youtube.com/embed/5fonu7btiT4?start=68" 
-          frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-          allowfullscreen></iframe>
-        <iframe v-bind:class="{'gallery-item': true, 'col-lg-2': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" width="560" height="315" src="https://www.youtube.com/embed/FSq37fLQPmU" frameborder="0" 
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-        </iframe>
-        <img v-bind:class="{'gallery-item': true, 'col-lg-2': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" src="./../assets/nino.jpg" />
-        <iframe v-bind:class="{'gallery-item': true, 'col-lg-2': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" width="560" height="315" src="https://www.youtube.com/embed/p3vS_BD1H5I" frameborder="0" 
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-        </iframe>
+    <div class="alternative-section gallery">
+      <h3>Hino do Elefante de Olinda</h3>
+      <div v-bind:class="{'row': widthScreen > 1024, 'col-lg-12': true, 'hymn': true}">
+        <div v-bind:class="{'col-lg-4': widthScreen > 1024, 'offset-2' : widthScreen > 1024}">
+          <p><i>Ao som dos clarins de Momo<br>
+          O povo aclama com todo ardor<br>
+          O Elefante exaltando as suas tradições<br>
+          E também seu esplendor<br>
+          Olinda, este meu canto<br>
+          Foi inspirado em teu louvor<br>
+          Entre confetes e serpentinas<br>
+          Venho te oferecer<br>
+          Com alegria o meu amor<br><br>
+          Olinda! Quero cantar a ti esta canção<br>
+          Teus coqueirais, o teu sol, o teu mar<br>
+          Faz vibrar meu coração, de amor a sonhar<br>
+          Minha Olinda sem igual<br>
+          Salve o teu Carnaval!</i></p>
+          <p>Clídio Nigro / Clóvis Vieira</p>
+        </div>
+        <div v-bind:class="{'col-lg-4': widthScreen > 1024, 'sound': true, 'smart': widthScreen <= 414}">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/tMa67fMqhyo" frameborder="0" 
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen></iframe>
+        </div>
       </div>
     </div>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h3>Galeria</h3>
+    <div v-bind:class="{'section-row': true, 'row col-lg-12 justify-content-between': widthScreen > 1024}">
+      <img v-bind:class="{'gallery-item': true, 'col-lg-2 first-gallery-item': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" src="./../assets/1953.jpg" />
+      <iframe v-bind:class="{'gallery-item': true, 'col-lg-2': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" width="560" height="315" src="https://www.youtube.com/embed/5fonu7btiT4?start=68" 
+        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen></iframe>
+      <iframe v-bind:class="{'gallery-item': true, 'col-lg-2': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" width="560" height="315" src="https://www.youtube.com/embed/FSq37fLQPmU" frameborder="0" 
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+      </iframe>
+      <img v-bind:class="{'gallery-item': true, 'col-lg-2': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" src="./../assets/nino.jpg" />
+      <iframe v-bind:class="{'gallery-item': true, 'col-lg-2': widthScreen > 1024, 'smart-gallery': widthScreen <= 1024}" width="560" height="315" src="https://www.youtube.com/embed/p3vS_BD1H5I" frameborder="0" 
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+      </iframe>
+    </div>
+    <div class="alternative-section gallery">
+      <h3>Siga o Elefante</h3>
+      <div v-bind:class="{'section-row': true}">
+        <a href="https://instagram.com/elefantedeolinda"><img v-bind:class="{'follow': true}" src="./../assets/instagram.png" /></a>
+        <a href="https://www.facebook.com/clubeelefantedeolindah"><img v-bind:class="{'follow': true}" src="./../assets/facebook.png" /></a>
+        <a href="https://twitter.com/ElefanteOlinda"><img v-bind:class="{'follow': true}" src="./../assets/twitter.png" /></a>
+        <a href="https://open.spotify.com/user/akua0hpd92ev9ieirigd0fmqy?si=z8KuWZ2sSzavGAkV9iy4vw"><img v-bind:class="{'follow': true}" src="./../assets/spotify.jpg" /></a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -155,19 +156,22 @@ li {
   max-width: 100%;
 }
 
-.gallery {
+.alternative-section {
   background-color: #A81317;
   color: white;
   padding-top: 0.5%;
   padding-bottom: 3%;
-  margin-top: 2%;
 }
 
-.gallery-time {
+.gallery {
+  margin-top: 3%;
+}
+
+.section-row {
   padding-top: 1%;
 }
 
-.gallery-time img {
+.section-row img {
   vertical-align: baseline;
 }
 
@@ -187,5 +191,14 @@ li {
 
 .smart-gallery {
   max-width: 240px;
+}
+
+.follow {
+  width: 50px;
+  height: 50px;
+  border-radius: 10px;
+  margin-top: 1%;
+  margin-left: 5px;
+  margin-right: 5px;
 }
 </style>
