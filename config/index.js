9e8bb7f116cd449e602c241d5dc6bc7738fe7fe9
luisdelgado <luishdelgados10@gmail.com>
2020-05-16 14:00:53 -0300

    Corrigindo problema do carregamento de imagens mudando path em config/index.js

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Painel',
+    assetsPublicPath: '/Painel/',
 
     /**
      * Source Maps
