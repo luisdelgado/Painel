53410152952e3a07202a7502f75587e30510ae3e
luisdelgado <luishdelgados10@gmail.com>
2020-05-19 20:44:03 -0300

    Adicionando HelloWorld

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Main from '@/components/Main'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Main',
-      component: Main
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
